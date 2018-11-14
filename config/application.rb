2a85b6aa066db6b6f4de29e541327a1450502e42
yoann benchetrit <benchetrityoann@gmail.com>
2018-11-14 12:10:31 +0200

    mister cocktail

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,15 @@ module RailsMisterCocktail
     config.generators.system_tests = nil
   end
 end
+
+const { environment } = require('@rails/webpacker')
+
+// Bootstrap 3 has a dependency over jQuery:
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+module.exports = environment
